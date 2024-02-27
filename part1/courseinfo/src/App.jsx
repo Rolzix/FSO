e04c31c19720172c5cac72974812ed2c8a5a1c17
Rolzi <aki.peltoniemi@gmail.com>
2024-02-27 18:51:10 +0200

    1.6 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,58 +1,25 @@
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>;
-};
-const Part = (props) => {
-  return (
-    <>
-      <li>{props.part.name}</li>
-    </>
-  );
-};
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.course.parts[0]}></Part>
-      <Part part={props.course.parts[1]}></Part>
-      <Part part={props.course.parts[2]}></Part>
-    </div>
-  );
-};
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {props.course.parts[0].exercises +
-        props.course.parts[1].exercises +
-        props.course.parts[2].exercises}
-    </p>
-  );
-};
+import { useState } from "react";
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [value, setValue] = useState(10);
+
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
+
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
-
 export default App;
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <div>give feedback</div>
+      <br></br>
+      <button onClick={() => setGood(good + 1)}>good</button>
+
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div>Statistics:</div>
+      <br></br>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  );
+};
+
+export default App;
