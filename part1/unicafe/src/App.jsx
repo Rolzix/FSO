22a8486c0d19e5c221a11a1740c4fe8e382d7757
Rolzi <aki.peltoniemi@gmail.com>
2024-02-27 19:10:28 +0200

    1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,15 +5,54 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
+  const handleClick = (e) => {
+    if (e.target.name === "good") {
+      const newGood = good + 1;
+      const newAll = all + 1;
+      const newPositive = (newGood / newAll) * 100;
+      setGood(newGood);
+      setAll(newAll);
+      setAverage((newGood * 1 + bad * -1) / newAll);
+      setPositive(newPositive);
+    }
+
+    if (e.target.name === "neutral") {
+      const newNeutral = neutral + 1;
+      const newAll = all + 1;
+      const newPositive = (good / newAll) * 100;
+      setNeutral(newNeutral);
+      setAll(newAll);
+      setAverage((good * 1 + bad * -1) / newAll);
+      setPositive(newPositive);
+    }
+
+    if (e.target.name === "bad") {
+      const newBad = bad + 1;
+      const newAll = all + 1;
+      setBad(newBad);
+      setAll(newAll);
+      setAverage((good * 1 + newBad * -1) / newAll);
+      setPositive((good / newAll) * 100);
+    }
+  };
   return (
     <div>
       <div>give feedback</div>
       <br></br>
-      <button onClick={() => setGood(good + 1)}>good</button>
+      <button name="good" onClick={handleClick}>
+        good
+      </button>
 
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button name="neutral" onClick={handleClick}>
+        neutral
+      </button>
+      <button name="bad" onClick={handleClick}>
+        bad
+      </button>
       <br></br>
       <br></br>
       <br></br>
@@ -22,6 +61,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive}%</div>
     </div>
   );
 };
