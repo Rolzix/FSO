1e8d725892538d86bdd0e7ee2a5deae537598b0a
Rolzi <aki.peltoniemi@gmail.com>
2024-02-28 15:27:03 +0200

    2.9 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [filtered, setFiltered] = useState(persons);
+  const [filterStatus, setFilterStatus] = useState(false);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  const filter = (e) => {
+    if (e.target.value === "") {
+      setFiltered(persons);
+      setFilterStatus(false);
+    } else {
+      const filtered = persons.filter((person) =>
+        person.name.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      setFiltered(filtered);
+      setFilterStatus(true);
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +37,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={filter} />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={(e) => setNewName(e.target.value)} />
@@ -29,11 +52,17 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      {filterStatus
+        ? filtered.map((person) => (
+            <div key={person.name}>
+              {person.name} {person.number}
+            </div>
+          ))
+        : persons.map((person) => (
+            <div key={person.name}>
+              {person.name} {person.number}
+            </div>
+          ))}
       ...
     </div>
   );
