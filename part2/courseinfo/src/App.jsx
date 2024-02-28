87d379f5a0113ad3a621a37eb81ba40f59d6ce5e
Rolzi <aki.peltoniemi@gmail.com>
2024-02-28 13:24:24 +0200

    2.1 done

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = ({ header }) => {
+  return <h3>{header.name}</h3>;
+};
+
+const Content = ({ content }) => {
+  const courseInfo = content.map((part) => (
+    <div key={part.id}>
+      {part.name} {part.exercises}
+    </div>
+  ));
+
+  return <>{courseInfo}</>;
+};
+
+const Course = (props) => {
+  return (
+    <>
+      <Header header={props.course} />
+      <Content content={props.course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
