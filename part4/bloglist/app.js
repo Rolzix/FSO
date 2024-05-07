1f214e59739dc9b36edb54a2fb50c0f52b28840b
Rolzi <aki.peltoniemi@gmail.com>
2024-05-07 20:21:10 +0300

    4.18 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 const cors = require("cors");
+const loginRouter = require("./controllers/login");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 
@@ -19,7 +20,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 
